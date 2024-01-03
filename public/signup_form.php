49557e8e8ff6bc9dfc822f6de73f927abde374d7
ShotaArima <rin.zoo.mamiya12345@gmail.com>
2024-01-03 17:12:48 +0900

    upload signup_form and create code

---
+++ public/signup_form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewpoint" content="witdh=device-width, initial-scale=1.0">
+        <title>ユーザ登録</title>
+    </head>
+    <body>
+        <h2>ユーザ登録フォーム</h2>
+        <form action="register.php" method="POST">
+            <p>
+                <label for="email">メールアドレス：</label>
+                <input type="email" name="email">
+            </p>
+            <p>
+                <label for="password">パスワード：</label>
+                <input type="password" name="password">
+            </p>
+            <p>
+                <label for="password_conf">パスワード確認：</label>
+                <input type="password" name="password_conf">
+            </p>
+            <p>
+                <input type="submit" value="新規登録">
+            </p>
+        </form>
+    </body>
+</html>
